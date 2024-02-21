84ab562839bdfc1f7bd6bdeea6bf16425abb1567
Anthony Lima Campelo <ahy.lm09x2@gmail.com>
2024-02-21 16:12:55 -0300

    Aula Modificando o tipo de dados

--- Aula 1.py
+++ Aula 1.py
@@ -1,11 +1,12 @@
 
 # Comentario simples // simple comment
-
+# Aula 1
 
 print("hello word")
 print('How are u?')
 print("What day is it?")
 
+# Aula 2
 '''
 Comentario de várias linhas // Multi-line comment
 
@@ -13,3 +14,19 @@ print("hello word")
 print('How are u?')
 print("What day is it?")
 '''
+
+# Aula 3
+'''
+Text => String/ str=("Texto")
+Numbers => Integer/ int=(1,2,3...N)
+Float => Quebrado/ float=(3,5 / 2,1 ...)
+Boolean type => Bool: True or False
+'''
+
+x = 3
+y = 4
+z = 5
+
+print(x)
+print(y)
+print(z)
