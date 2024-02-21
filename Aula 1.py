1fdaa01d45ea3e15288d46929f195eeddd4a2781
Anthony Lima Campelo <ahy.lm09x2@gmail.com>
2024-02-21 15:55:07 -0300

    Aula 2 - comentarios

--- Aula 1.py
+++ Aula 1.py
@@ -1,3 +1,15 @@
+
+# Comentario simples // simple comment
+
+
 print("hello word")
 print('How are u?')
 print("What day is it?")
+
+'''
+Comentario de várias linhas // Multi-line comment
+
+print("hello word")
+print('How are u?')
+print("What day is it?")
+'''
