22b20947224826812ef7581fc0c09bc3658eed04
anthonyL666 <ahy.lm09x2@gmail.com>
2024-02-27 10:36:14 -0300

    for - Nested Loops

--- Aulas.py
+++ Aulas.py
@@ -235,3 +235,24 @@ for enviar in range(3):
         break
 else:
     print("Compra não autorizada")
+
+### Nested Loops
+    
+'''
+for loop nested
+
+       Outer loop
+        inner loop
+'''
+
+for numero1 in range(5):
+    print(numero1)
+    for numero2 in range(5):
+        print(numero2)
+
+
+for numero1 in range(1,6):
+    print('Produto ' + str(numero1))
+    for numero2 in range(1,10):
+        print(numero1, numero2)
+
