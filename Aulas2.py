34deade0993ddd1eb87c4400671656c8ab38cb92
Anthony Lima Campelo <ahy.lm09x2@gmail.com>
2024-02-28 14:47:37 -0300

    Intro Functions

---
+++ Aulas2.py
@@ -0,0 +1,26 @@
+"""
+            Function       |         Module              |           Package                     |       Library
+   
+    Item    
+        Maça           ----                          ----                                    ---- 
+            - Peso         |                             |                                       |
+            - Valor        |  Sessão Frutas e verduras   |       Supermercado A                  |
+            - Cor          |                             |                                       |
+        Banana              >>>   Sessão beleza           >>>    Supermercado B                   >>>   Hypermarket
+            - Peso         |                             |                                       |
+            - Valor        |      Sessão carnes          |       Supermercado C                  |
+            - Cor          |                             |                                       |
+                       ----                          ----                                    ----
+"""
+
+# Functions 
+    # DRY - Don't repeat yourself.
+
+def boasVindas ():
+    print('Ola Marcos!')
+    print("Temos 5 laptops em estoque!")
+
+boasVindas()
+
+
+
--- tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-produto = input("Digite o valor do produto: ")
-porcentagem = 10/100
-
-if produto > 20:
-    produto += (porcentagem * produto)
-    print(f'O valor do produto é de {produto}')
